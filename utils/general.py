2e62ccc86f924c8e0f7bbaf5d6be502832c3e5f1
Frank Verhoef <fsa.verhoef@chello.nl>
2023-02-16 22:33:29 +0100

    General utility functions

---
+++ utils/general.py
@@ -0,0 +1,28 @@
+###
+### General util functions
+###
+
+def savename(args):
+    name = args.save
+    if args.model == "bert":
+        name += "bert"
+    elif args.model == "seq2seq":
+        name += "_seq2seq_{}_{}_I{}_E{}_H{}".format(
+            args.encoder,
+            args.decoder,
+            args.vocab_size,
+            args.embedding_size,
+            args.hidden_size
+        )
+    return name
+
+def print_params(model):
+    print("Model parameters")
+    for p in model.parameters():
+        print("\t{:<30} {}".format(str(p.data.shape), p.data.norm()))
+
+def print_grads(model):
+    print("Gradients")
+    for p in model.parameters():
+        print("\t{:<30} {}".format(str(p.grad.shape), p.grad.norm()))
+
