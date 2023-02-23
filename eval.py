4659c1ad8c9221b53898ac570db252afee5ec49f
Frank Verhoef <fsa.verhoef@chello.nl>
2023-02-23 21:08:57 +0100

    update eval with BERT variants

--- eval.py
+++ eval.py
@@ -5,14 +5,18 @@ to run with defaults
 import torch
 import torch.nn as nn
 import random
+from tqdm import tqdm
+from torcheval.metrics.functional import binary_confusion_matrix, binary_accuracy, binary_f1_score
 
 from transformers import AutoTokenizer
 from dataset.msc_binary import MSC_Turn_Facts
 from models.persona_extractor import PersonaExtractor
-from models.bert_classifier import BertClassifier
+from models.bert_classifier import BertClassifier, FrozenBert, PrefixBert
 from dataset.msc_summary import MSC_Turns, extra_tokens
 from dataset.vocab import Vocab, PAD_TOKEN, START_TOKEN, END_TOKEN
 
+import transformers
+transformers.utils.logging.set_verbosity_error
 
 def eval(model, dataloader, vocab, decoder_max):
 
@@ -31,10 +35,11 @@ def eval(model, dataloader, vocab, decoder_max):
 
 def eval_bert(model, dataloader, tokenizer):
 
-    num_correct, num_eval = 0, 0
+    all_labels = []
+    all_preds = []
     model.eval()
 
-    for batch in iter(dataloader):
+    for batch in tqdm(iter(dataloader)):
 
         (input_ids, attention_mask, token_type_ids), y = batch
 
@@ -42,11 +47,19 @@ def eval_bert(model, dataloader, tokenizer):
             logits = model(input_ids, attention_mask, token_type_ids)
             pred = logits.argmax(dim=1)
             
-        num_correct += pred.eq(y).sum().item()
-        num_eval += len(y)
-        print_bert_predictions(input_ids, y, pred, tokenizer)
+        all_labels.append(y)
+        all_preds.append(pred)
+        # print_bert_predictions(input_ids, y, pred, tokenizer)
 
-    return {"test_acc": num_correct/num_eval}
+    all_labels = torch.cat(all_labels)
+    all_preds = torch.cat(all_preds)
+    stats = {
+        "test_acc": binary_accuracy(all_preds, all_labels).item(),
+        "f1": binary_f1_score(all_preds, all_labels).item(),
+        "cm": binary_confusion_matrix(all_preds, all_labels).tolist()
+    }
+
+    return stats
 
 
 def print_bert_predictions(xs, ys, pred, tokenizer):
@@ -89,12 +102,17 @@ if __name__ == "__main__":
     parser.add_argument("--decoder", type=str, default="lstm", help="Decoder model")
     parser.add_argument("--decoder_max", type=int, default=20, help="Max number of tokens to generate with decoder")
 
+    # Bert variants
+    parser.add_argument("--prefix_size", type=int, default=0, help="Insert prefix in BERT")
+
     # Dataset
     parser.add_argument("--datadir", type=str, default="/Users/FrankVerhoef/Programming/PEX/data/", help="Datadir")
     parser.add_argument("--testdata", type=str, default="msc/msc_personasummary/session_1/train.txt", help="Dataset file for testing")
     parser.add_argument("--vocab_size", type=int, default=4000, help="Max number of unique token (excluding special tokens)")
     parser.add_argument("--test_samples", type=int, default=10, help="Max number of test samples")
-    
+    parser.add_argument("--persona_tokens", type=bool, default=False, help="Whether to insert special persona token before each dialogue turn")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
+
     args = parser.parse_args()
 
     random.seed(args.seed)
@@ -102,10 +120,11 @@ if __name__ == "__main__":
 
     if args.task == 'classify':
 
-        tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
-        num_added_toks = tokenizer.add_tokens(extra_tokens)
-
-        model = BertClassifier()
+        tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        if args.persona_tokens:
+            num_added_toks = tokenizer.add_tokens(extra_tokens)
+        
+        model = PrefixBert('bert-base-uncased', prefix_size=args.prefix_size)
         model.bert.resize_token_embeddings(len(tokenizer))
         testdata = MSC_Turn_Facts(args.datadir + args.testdata, tokenizer, len_context=2, max_samples=args.test_samples)
 
@@ -134,14 +153,15 @@ if __name__ == "__main__":
         }
         model = PersonaExtractor(args.encoder, encoder_opts, args.decoder, decoder_opts, start_token=vocab.tok2ind[START_TOKEN])
     
+    print("Loading model from {}".format(args.checkpoint_dir + args.load))
     model.load_state_dict(torch.load(args.checkpoint_dir + args.load))
 
-    test_loader = torch.utils.data.DataLoader(dataset=testdata, batch_size=1, shuffle=True, collate_fn=testdata.batchify)
+    test_loader = torch.utils.data.DataLoader(dataset=testdata, batch_size=args.batch_size, shuffle=True, collate_fn=testdata.batchify)
 
     if args.model == 'bert':
         eval_stats = eval_bert(model, test_loader, tokenizer)
         for k, v in eval_stats.items():
-            print("{:<8}: {}".format(k, v))
+            print("{:<10}: {}".format(k, v))
     elif args.model == 'seq2seq':
         eval(model, test_loader, vocab, decoder_max=args.decoder_max)
 
