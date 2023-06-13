corrected calculation of perplexity

dataset/msc_sessions.py
@@ -7,7 +7,6 @@ import torch
 from torchmetrics import SacreBLEUScore, BLEUScore,  MeanMetric
 from torchmetrics.text.rouge import ROUGEScore
 from torchmetrics.text.bert import BERTScore
-from torchmetrics.text.perplexity import Perplexity
 import evaluate
 
 from torch.utils.data import Dataset
@@ -33,7 +32,6 @@ class MSC_Metrics:
         self.responses = []
         self.targets = []
         self.perc_truncated_tokens = torch.tensor([])
-        self.perplexity = Perplexity(ignore_index=ignore_index).to(device)
         self.sacreblue4_score = SacreBLEUScore(n_gram=4)
         self.bleu2_score = BLEUScore(n_gram=2, smooth=True)
         self.bleu4_score = BLEUScore(n_gram=4, smooth=True)
@@ -53,9 +51,6 @@ class MSC_Metrics:
                 torch.maximum(input_batch.num_original_tokens, torch.ones_like(input_batch.num_original_tokens)),
             )
             self.perc_truncated_tokens = torch.cat([self.perc_truncated_tokens, avg_truncation], dim=0)
-        len_scores = len(output_batch.scores)
-        scores = torch.cat(output_batch.scores, dim=-1).view(label_batch.input_ids.shape[0], len_scores, -1)
-        self.perplexity.update(scores, label_batch.input_ids[:, :len_scores].to(scores.device))
         self.sacreblue4_score.update(responses, targets)
         self.bleu2_score.update(responses, targets)
         self.bleu4_score.update(responses, targets)
@@ -86,7 +81,6 @@ class MSC_Metrics:
 
         stats = {
             "truncation": self.perc_truncated_tokens.mean().item() if len(self.perc_truncated_tokens) > 0 else 0,
-            "perplexity": self.perplexity.compute().item(),
             "sacreblue_4": self.sacreblue4_score.compute().item(),
             "bleu_2": self.bleu2_score.compute().item(), 
             "bleu_4": self.bleu4_score.compute().item(),             
@@ -651,12 +645,12 @@ if __name__ == "__main__":
     if session == 1:
         version = ['both', 'revised']
         session = '-'.join(['1'] + version)
-    speaker_prefixes = None #['<other>', '<self>']
-    sessionbreak_token = None #'<sessionbreak>'
+    speaker_prefixes = ['<other>', '<self>']
+    sessionbreak_token = '<sessionbreak>'
     add_tokens = None #speaker_prefixes if sessionbreak_token is None else speaker_prefixes + [sessionbreak_token]
     include_persona = True
     include_history = True
-    input_order = INPUT_ORDER_OPTIONS[1]
+    input_order = INPUT_ORDER_OPTIONS[0]
     max_samples = 5
 
     augmented = False

models/bart_extractor.py
@@ -13,6 +13,7 @@ class ConditionalFactLoss(nn.Module):
     def __init__(self, nofact_token_id, ignore_index=-100, lm_weight=0.5):
         super().__init__()
         self.nofact_token_id = nofact_token_id
+        self.ignore_index = ignore_index
         self.nllloss = nn.NLLLoss(ignore_index=ignore_index, reduction='none')
         self.lm_weight = lm_weight
 
@@ -50,6 +51,7 @@ class ExtractedFactLoss(nn.Module):
         assert True if clf_loss is None else clf_loss == 'reweighted', f"Invalid value for clf_loss '{clf_loss}'"
         self.nofact_token_id = nofact_token_id
         self.clf_loss = clf_loss
+        self.ignore_index = ignore_index
         if nofact_weight is not None:
             weight = torch.ones(num_tokens)
             weight[nofact_token_id] = nofact_weight
@@ -194,7 +196,7 @@ class BartExtractor(nn.Module):
         token_acc = (token_correct.sum() / ignore_mask.sum()).item() 
 
         # LM perplexity
-        ppl = perplexity(preds=lm_logprobs, target=y, ignore_index=self.bart.config.pad_token_id).item()
+        ppl = perplexity(preds=lm_logprobs, target=y, ignore_index=criterion.ignore_index).item()
 
         stats = {
             "loss": loss.item(),
@@ -400,8 +402,7 @@ if __name__ == "__main__":
         model.load_state_dict(torch.load(loadpath, map_location=torch.device(args.device)))
 
     # Test forward
-    fact_logprobs, lm_logprobs = model(batch['input_ids'], batch['attention_mask'], batch['labels'])
-    pred_fact = fact_logprobs.argmax(dim=-1) 
+    lm_logprobs = model(batch['input_ids'], batch['attention_mask'], batch['labels'])
     preds = lm_logprobs.argmax(dim=-1)
     response = tokenizer.batch_decode(preds)
 
@@ -409,7 +410,7 @@ if __name__ == "__main__":
         print('-' * 40)
         print(data[i][0])
         print(data[i][1])
-        print(response[i] if pred_fact[i] else args.nofact_token)
+        print(response[i])
 
     # Test loss function and valid_step
     valid_stats = model.valid_step(batch, criterion, device=args.device)